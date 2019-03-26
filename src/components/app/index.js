21d1b10d3bc106bc37e58a5ecadd2af965cc733b
Joaquín Lobo Rodríguez <jloborod@gmail.com>
2019-03-26 23:28:33 +0100

    Initial commit :)

---
+++ src/components/app/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div className="app">Hello there!</div>;
+
+export default App;
---
+++ webpack.config.babel.js
@@ -0,0 +1,45 @@
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+
+export default {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  }
+};

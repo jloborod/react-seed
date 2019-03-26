530f62cbe0cf039b65425c0bfeadb6ddb0c97953
Joaquín Lobo Rodríguez <jloborod@gmail.com>
2019-03-26 23:44:28 +0100

    added react hot loader

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import { hot } from 'react-hot-loader/root';
 
 const App = () => <div className="app">Hello there!</div>;
 
-export default App;
+export default hot(App);

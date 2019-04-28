30257be46ff0baa9f89e3a97e91fbb03ce872afd
Joaquín Lobo Rodríguez <jloborod@gmail.com>
2019-04-28 12:19:19 +0100

    added eslint to the project with recommended config from eslint and react

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-const App = () => <div className="app">Hello there!</div>;
+const App = () => <div className='app'>Hello there!</div>;
 
 export default hot(App);
